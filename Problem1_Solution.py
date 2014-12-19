1265a0af98cc5782ca34c441d9b1271dd1a5553e
anujsethi12 <anujsethi12@gmail.com>
2014-12-19 15:12:26 -0500

    Update Problem1_Solution.py

--- Problem1_Solution.py
+++ Problem1_Solution.py
@@ -2,11 +2,13 @@ print ("Program to count Lines in CSV");
 
 import csv
 
+##Open and read bookings.csv and count no. of lines
 print ("\nLines in bookings.csv")
 with open('bookings.csv', 'r') as csv_bookings:
     count_bookings = sum(1 for line in csv_bookings)
 print (count_bookings);
 
+##Open and read searches.csv and count no. of lines
 print ("\nLines in searches.csv")
 with open('searches.csv', 'r') as csv_searches:
     count_searches = sum(1 for line in csv_searches)
